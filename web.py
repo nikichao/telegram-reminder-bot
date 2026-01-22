dcbf8111022100dbd6230a71526136eade719ed5
nikichao <32010706+nikichao@users.noreply.github.com>
2026-01-22 20:12:27 +0400

    Update web.py

--- web.py
+++ web.py
@@ -1,13 +1,13 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('')
-def home()
-    return 🤖 Telegram Bot работает!
+@app.route('/')
+def home():
+    return "Telegram Bot работает на Railway!"
 
-@app.route('health')
-def health()
-    return OK, 200
+@app.route('/health')
+def health():
+    return "OK", 200
 
-if __name__ == __main__
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
